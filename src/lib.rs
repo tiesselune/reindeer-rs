5cba4f4981e4ac995cd13e620da5fc4d3702cebb
Julien SOSTHENE <tiesse@pm.me>
2022-06-10 17:42:59 +0200

    Re-export some utilities from serde and sled

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 pub mod entity;
 pub mod relation;
 pub use entity::Entity;
+pub use entity::AutoIncrementEntity;
+pub use sled::Db;
+pub use sled::open;
+pub use serde_derive::{Serialize,Deserialize};
+pub use relation::DeletionBehaviour;
 
 #[cfg(test)]
 mod tests {
